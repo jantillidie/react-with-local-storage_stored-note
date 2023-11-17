0f2c411e6798eb2b6dc7f9e5fb69881bc0522ea2
Jan Maaß <git@antipost.de>
2023-11-17 14:50:28 +0100

    Correct solution

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Note from "./components/Note";
 import "./styles.css";
 
 export default function App() {
-  const [note, setNote] = useLocalStorageState("");
-  const [font, setFont] = useLocalStorageState("system-ui");
+  const [note, setNote] = useLocalStorageState("note", { defaultValue: '' });
+  const [font, setFont] = useLocalStorageState("font", { defaultValue: 0 });
 
   function handleNoteChange(newNote) {
     setNote(newNote);
